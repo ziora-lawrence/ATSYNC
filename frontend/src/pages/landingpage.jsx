c122c61ed9f77b6f1972c386e22e36623dee3477
ziora-lawrence <danieliwuji28@gmail.com>
2026-04-21 23:33:01 +0100

    just the first section

--- frontend/src/pages/landingpage.jsx
+++ frontend/src/pages/landingpage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import './landingpage.css'
 
 const Landingpage = () => {
   return (
-    <div className='landing-container'>
-        <div>
-            <h1>
-                Landing page
-            </h1>
-        </div>
+    <div className='landing-page'>
+        <section className='hero'>
+            <div className='hero-text'>
+                <h1>
+                    The Future of Client<br/>
+                    Relations for
+                </h1>
+                <p>Ai powered communication that automates onboarding and<br/>  updates, so you can focus on the work</p>
+                <div className='hero-buttons'>
+                    <button className='get-early-access'>Get Early Access</button>
+                    <button className='watch-demo'>Watch Demo</button>
+                </div>
+            </div>
+            <div className='hero-image'>
+                <img src="" alt="idk what should be here tbh"/>
+            </div>
+        </section>
     </div>
   )
 }
